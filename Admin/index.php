1340dfef620034dd8fc2417d325d2cc3bff906f1
LinhVu <Linhvt@example.com>
2023-02-12 10:55:40 +0700

    add

--- Admin/index.php
+++ Admin/index.php
@@ -1,205 +1,196 @@
 <?php
-	include 'view/header.php';
-	include './model/pdo.php';
-	include './model/danhmuc.php';
-	include './model/hanghoa.php';
-	$listCates = select_cate();
-	
-	include './view/product.php';
-	if(isset($_GET['target'])) {
-		$variable = $_GET['target'];
-		switch ($variable) {
-			case 'product':
-				if(isset($_GET['iddm']) && ($_GET['iddm'] > 0)) {
-					$iddm=$_GET['iddm'];
-					$list_product=select_items_search("",$iddm);
-					
-					$name_dm=loadname_item($iddm);
-				}
-				
-				include './view/product.php';
-				break;
-			case 'product_ct':
-				if(isset($_GET['id']) && ($_GET['id'] > 0)) {
-					$id=$_GET['id'];
-					
-					$item = loadOne_item($_GET['id']);
-					extract($item);
-					$items=load_products($id,$ma_loai);
-				}
-				
-				include './view/product_ct.php';
-				break;
-			case 'addmoveCate': 
-				include './Danhmuc/add.php';
-				break;
-			case 'addCate':
-				if(isset($_POST['addNewCate']) && $_POST['addNewCate']) {
-					$ten_loai = $_POST['tenloai'];
-					insert_cate($ten_loai);
-					$thongbao = "Thêm mới danh mục thành công!";
-				}
-				$listCates = select_cate();
-				include './Danhmuc/list.php';
-				break;
-			case 'listCate' :
-				$listCates = select_cate();
-				include './Danhmuc/list.php';
-				break;
-			
-			case 'deleteCate' : 
-				if(isset($_GET['id']) && ($_GET['id'] > 0)) {
-					delete_cate($_GET['id']);
-					$thongbao_xoa = "Xóa thành công !!";
-				}
+include 'view/header.php';
+include './model/pdo.php';
+include './model/danhmuc.php';
+include './model/hanghoa.php';
+$listCates = select_cate();
 
-				$listCates = select_cate();
-				include './Danhmuc/list.php';
-				break;
-			case 'editCate': 
-				if(isset($_GET['id']) && ($_GET['id'] > 0)) {
-					$item = loadOne_cate($_GET['id']);
-				}
-				$item = $item;
-				include './Danhmuc/update.php';
-				break;
-			case 'editedCate': 
-				if(isset($_POST['updateCate']) && $_POST['updateCate']) {
-					$id = $_POST['id'];
-					$ten_loai = $_POST['tenloai'];
-					update_cate($id, $ten_loai);
-					$thongbao = "Cập nhật danh mục thành công!";
-				}
-				$listCates = select_cate();
-				include './Danhmuc/list.php';
-				break;
-			
-			case 'addmoveItems': 
-				$listCates = select_cate();
-				include './Product/add.php';
-				break;
-			case 'addItems':
-				if(isset($_POST['addNewItem']) && $_POST['addNewItem']) {
-					$ten_loai = $_POST['nameitem'];
-					$gia = $_POST['priceitem'];
-					$discount = $_POST['discountitem'];
-					$mota = $_POST['descitem'];
-					$view = $_POST['views'];
-					$date = $_POST['date'];
-					$ma_loai = $_POST['maloai'];
-					
-					// if($_POST['nameitem']==null){
-					// 	$thongbao1 = "Vui lòng nhập tên sản phẩm !";	
-					// }
-					// if($_POST['priceitem']==null || $_POST['priceitem']<0){
-					// 	$thongbao2 = "Vui lòng nhập giá sản phẩm là số dương !";	
-					// }
-					// if($_POST['discountitem']==null || $_POST['discountitem']<0){
-					// 	$thongbao3 = "Vui lòng nhập giá sale sản phẩm là số dương !";	
-					// }
-					// if($_POST['date']==null){
-					// 	$thongbao4 = "Vui lòng nhập ngày nhập sản phẩm !";	
-					// }
-					// if($_POST['descitem']==null){
-					// 	$thongbao5 = "Vui lòng nhập mô tả sản phẩm !";	
-					// }
-					// if($_POST['views']==null || $_POST['views']<0){
-					// 	$thongbao6 = "Vui lòng nhập số lượt xem là số dương sản phẩm !";	
-					// }
 
-					$anh_dai_dien = isset($_FILES['imageitem']) ? $_FILES['imageitem'] : '';
-					$save_url = '';
-					if($anh_dai_dien['size'] > 0 && $anh_dai_dien['size'] < 500000) {
-						$photo_folder = 'Image/';
-						$photo_file = uniqid() . $anh_dai_dien['name'];
+if (isset($_GET['target'])) {
+	$variable = $_GET['target'];
+	switch ($variable) {
 
-						$file_se_luu = $anh_dai_dien['tmp_name'];
-						$url = $photo_folder . $photo_file;
+		case 'product_ct':
+			if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+				$id = $_GET['id'];
 
-						if(move_uploaded_file($file_se_luu, $url)) {
-							$save_url = $url;
-						}
+				$item = loadOne_item($_GET['id']);
+				extract($item);
+				$items = load_products($id, $ma_loai);
+			}
+
+			include './view/product_ct.php';
+			break;
+		case 'addmoveCate':
+			include './Danhmuc/add.php';
+			break;
+		case 'addCate':
+			if (isset($_POST['addNewCate']) && $_POST['addNewCate']) {
+				$ten_loai = $_POST['tenloai'];
+				insert_cate($ten_loai);
+				$thongbao = "Thêm mới danh mục thành công!";
+			}
+			$listCates = select_cate();
+			include './Danhmuc/list.php';
+			break;
+		case 'listCate':
+			$listCates = select_cate();
+			include './Danhmuc/list.php';
+			break;
+
+		case 'deleteCate':
+			if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+				delete_cate($_GET['id']);
+				$thongbao_xoa = "Xóa thành công !!";
+			}
+
+			$listCates = select_cate();
+			include './Danhmuc/list.php';
+			break;
+		case 'editCate':
+			if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+				$item = loadOne_cate($_GET['id']);
+			}
+			$item = $item;
+			include './Danhmuc/update.php';
+			break;
+		case 'editedCate':
+			if (isset($_POST['updateCate']) && $_POST['updateCate']) {
+				$id = $_POST['id'];
+				$ten_loai = $_POST['tenloai'];
+				update_cate($id, $ten_loai);
+				$thongbao = "Cập nhật danh mục thành công!";
+			}
+			$listCates = select_cate();
+			include './Danhmuc/list.php';
+			break;
+
+		case 'addmoveItems':
+			$listCates = select_cate();
+			include './Product/add.php';
+			break;
+		case 'addItems':
+			if (isset($_POST['addNewItem']) && $_POST['addNewItem']) {
+				$ten_loai = $_POST['nameitem'];
+				$gia = $_POST['priceitem'];
+				$discount = $_POST['discountitem'];
+				$mota = $_POST['descitem'];
+				$view = $_POST['views'];
+				$date = $_POST['date'];
+				$ma_loai = $_POST['maloai'];
+
+				// if($_POST['nameitem']==null){
+				// 	$thongbao1 = "Vui lòng nhập tên sản phẩm !";	
+				// }
+				// if($_POST['priceitem']==null || $_POST['priceitem']<0){
+				// 	$thongbao2 = "Vui lòng nhập giá sản phẩm là số dương !";	
+				// }
+				// if($_POST['discountitem']==null || $_POST['discountitem']<0){
+				// 	$thongbao3 = "Vui lòng nhập giá sale sản phẩm là số dương !";	
+				// }
+				// if($_POST['date']==null){
+				// 	$thongbao4 = "Vui lòng nhập ngày nhập sản phẩm !";	
+				// }
+				// if($_POST['descitem']==null){
+				// 	$thongbao5 = "Vui lòng nhập mô tả sản phẩm !";	
+				// }
+				// if($_POST['views']==null || $_POST['views']<0){
+				// 	$thongbao6 = "Vui lòng nhập số lượt xem là số dương sản phẩm !";	
+				// }
+
+				$anh_dai_dien = isset($_FILES['imageitem']) ? $_FILES['imageitem'] : '';
+				$save_url = '';
+				if ($anh_dai_dien['size'] > 0 && $anh_dai_dien['size'] < 500000) {
+					$photo_folder = 'Image/';
+					$photo_file = uniqid() . $anh_dai_dien['name'];
+
+					$file_se_luu = $anh_dai_dien['tmp_name'];
+					$url = $photo_folder . $photo_file;
+
+					if (move_uploaded_file($file_se_luu, $url)) {
+						$save_url = $url;
 					}
-					//if($ten_loai && $gia >0 && $discount >0 && $mota && $view >0 && $date){
-						insert_item($ten_loai, $gia, $discount, $save_url, $date, $mota, $view, $ma_loai); 
-						$thongbao = "Thêm mới sản phẩm  thành công !";	
-						
-					//}
 				}
+				//if($ten_loai && $gia >0 && $discount >0 && $mota && $view >0 && $date){
+				insert_item($ten_loai, $gia, $discount, $save_url, $date, $mota, $view, $ma_loai);
+				$thongbao = "Thêm mới sản phẩm  thành công !";
 
-				$listItems = select_items();
-				
-				include './Product/list.php';
-				break;
-			case 'listItems' :
-				if(isset($_POST['listok']) && $_POST['listok']){
-					$keyw = $_POST['keyw'];
-					$ma_loai = $_POST['maloai'];
-				}else{
-					$keyw="";
-					$ma_loai =0;
-				}
-				$listCates = select_cate();
-				$listItems = select_items_search($keyw,$ma_loai);
-				include './Product/list.php';
-				break;
-			case 'deleteItem' : 
-				if(isset($_GET['id']) && ($_GET['id'] > 0)) {
-					delete_item($_GET['id']);
-					$thongbao_delete = "Xóa thành công !!";
-				}
+				//}
+			}
 
-				$listItems = select_items();
-				include './Product/list.php';
-				break;
-			case 'editItem': 
-				if(isset($_GET['id']) && ($_GET['id']>0)) {
-					$item = loadOne_item($_GET['id']);
-					
-				}
-				$listCates = select_cate();
-				include './Product/update.php';
-				// include './Product/list.php';
-				break;
-			case 'editedItem': 
-				if(isset($_POST['updateitem']) && $_POST['updateitem']) {
-					$id = $_POST['id'];
-					$ten_loai = $_POST['nameitem'];
-					$gia = $_POST['priceitem'];
-					$discount = $_POST['discountitem'];
-					$mota = $_POST['descitem'];
-					$view = $_POST['views'];
-					$date = $_POST['date'];
-					$ma_loai = $_POST['maloai'];
+			$listItems = select_items();
 
-					$anh_dai_dien = isset($_FILES['imageitem']) ? $_FILES['imageitem'] : '';
-					$save_url = '';
-					if($anh_dai_dien['size'] > 0 && $anh_dai_dien['size'] < 500000) {
-						$photo_folder = 'Image/';
-						$photo_file = uniqid() . $anh_dai_dien['name'];
+			include './Product/list.php';
+			break;
+		case 'listItems':
+			if (isset($_POST['listok']) && $_POST['listok']) {
+				$keyw = $_POST['keyw'];
+				$ma_loai = $_POST['maloai'];
+			} else {
+				$keyw = "";
+				$ma_loai = 0;
+			}
+			$listCates = select_cate();
+			$listItems = select_items_search($keyw, $ma_loai);
+			include './Product/list.php';
+			break;
+		case 'deleteItem':
+			if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+				delete_item($_GET['id']);
+				$thongbao_delete = "Xóa thành công !!";
+			}
 
-						$file_se_luu = $anh_dai_dien['tmp_name'];
-						$url = $photo_folder . $photo_file;
+			$listItems = select_items();
+			include './Product/list.php';
+			break;
+		case 'editItem':
+			if (isset($_GET['id']) && ($_GET['id'] > 0)) {
+				$item = loadOne_item($_GET['id']);
 
-						if(move_uploaded_file($file_se_luu, $url)) {
-							$save_url = $url;
-						}
+			}
+			$listCates = select_cate();
+			include './Product/update.php';
+			// include './Product/list.php';
+			break;
+		case 'editedItem':
+			if (isset($_POST['updateitem']) && $_POST['updateitem']) {
+				$id = $_POST['id'];
+				$ten_loai = $_POST['nameitem'];
+				$gia = $_POST['priceitem'];
+				$discount = $_POST['discountitem'];
+				$mota = $_POST['descitem'];
+				$view = $_POST['views'];
+				$date = $_POST['date'];
+				$ma_loai = $_POST['maloai'];
+
+				$anh_dai_dien = isset($_FILES['imageitem']) ? $_FILES['imageitem'] : '';
+				$save_url = '';
+				if ($anh_dai_dien['size'] > 0 && $anh_dai_dien['size'] < 500000) {
+					$photo_folder = 'Image/';
+					$photo_file = uniqid() . $anh_dai_dien['name'];
+
+					$file_se_luu = $anh_dai_dien['tmp_name'];
+					$url = $photo_folder . $photo_file;
+
+					if (move_uploaded_file($file_se_luu, $url)) {
+						$save_url = $url;
 					}
-					update_item($id, $ten_loai, $gia, $discount, $save_url, $date, $mota, $view, $ma_loai);
-					$thongbao_update = "Cập nhật sp thành công!";
 				}
-				$listItems = select_items();
-				include './Product/list.php';
-				break;
-			default:
-				// $listItems = select_items();
-				include 'body.php';
-				break;
-		}
-	}else {
-		$listbody = select_items_body();
-		include './view/body.php';
-		//break;
+				update_item($id, $ten_loai, $gia, $discount, $save_url, $date, $mota, $view, $ma_loai);
+				$thongbao_update = "Cập nhật sp thành công!";
+			}
+			$listItems = select_items();
+			include './Product/list.php';
+			break;
+		default:
+			// $listItems = select_items();
+			include './view/body.php';
+			break;
 	}
-	include './view/footer.php';	
- ?>
+} else {
+	$listbody = select_items_body();
+	include './view/body.php';
+	//break;
+}
+include './view/footer.php';
+?>
--- Admin/view/body.php
+++ Admin/view/body.php
@@ -1,87 +1,84 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 	<link rel="stylesheet" href="css/body.css">
-	
+
+
 </head>
+
 <body>
 	<div class="body_main">
-			<div class="left">
-				<div class="slideshow">
-					<img src="img_product/product1.jpg" alt="">
-				</div>
-				<div class="product">
+		<div class="left">
+			<div class="slideshow">
+				<img src="img_product/product1.jpg" alt="">
+			</div>
+			<div class="product">
 				<section class="main-menu">
-	<?php
-	
-		foreach ($listbody as $hanghoa) { 
-			extract($hanghoa); ?>
-		 	<div class="box">
-		 		<div class="main-type">
-		 			<?php
-		 				$item = loadOne_cate($ma_loai);
-		 				extract($item);
-		 			 ?>
-		 			 <?= $ten_loai ?>
-		 		</div>
-				
-		 		<div class="img_product" >
-		 			<a href="./index.php?target=product_ct&id=<?= $ma_hanghoa ?> ">
-		 				<img class="main-image" src="<?= $hinh ?>" alt="">
-		 			</a>
-		 		</div>
-		 		<div class="main-info">
-				 <span class="main-price">
-		 				 <?= $don_gia ?>VND
-		 		</span>
-				
-		 			<h3 class="main-title">
-		 				<a href="./index.php?target=product_ct&id=<?= $ma_hanghoa ?> ">
-		 					<?= $ten_hanghoa ?>
-		 				</a>
-		 			</h3>
-		 			
-		 		</div>
-		 	</div>
-		<?php }  ?>
-</section>
-				</div>
-			</div>
-			<div class="right">
-				<div class="box">
-					<h3>TÀI KHOẢN</h3>
-					<form action="" method="post">
-						<p>Tên đăng nhập</p>
-						<input type="text" name="name">
-						<p>Mật khẩu</p>
-						<input type="password" name="password" id=""> <br>
-						<input type="checkbox" name="checkbox" id="">Ghi nhớ tài khoản
-						<input type="submit" value="Đăng nhập">
-					</form>
-					<ul>
-						<li>Quên mật khẩu</li>
-						<li>Đăng ký thành viên</li>
-					</ul>
-				</div>
-				<div class="box">
-					<h3>DANH MỤC</h3>
 					<?php
-					foreach($listCates as $name){
-						extract($name);
-						$link_product="index.php?target=product&id=".$ma_loai;
-						echo '<li><a class="link_product" href="'.$link_product.'">'.$ten_loai.'</li>';
-					}
-					?>
-				</div>
-				<div class="box">
-					<h3>TOP 10 YÊU THÍCH</h3>
-				</div>
+
+					foreach ($listbody as $hanghoa) {
+						extract($hanghoa); ?>
+						<div class="box">
+							<div class="main-type">
+								<?php
+								$item = loadOne_cate($ma_loai);
+								extract($item);
+								?>
+								<?= $ten_loai ?>
+							</div>
+
+							<div class="img_product">
+								<a href="./index.php?target=product_ct&id=<?= $ma_hanghoa ?> ">
+									<img class="main-image" src="<?= $hinh ?>" alt="">
+								</a>
+							</div>
+							<div class="main-info">
+								<span class="main-price">
+									<?= $don_gia ?>VND
+								</span>
+
+								<h3 class="main-title">
+									<a href="./index.php?target=product_ct&id=<?= $ma_hanghoa ?> ">
+										<?= $ten_hanghoa ?>
+									</a>
+								</h3>
+
+							</div>
+						</div>
+					<?php } ?>
+				</section>
 			</div>
+		</div>
+		<div class="right">
+			<div class="box">
+				<h3>TÀI KHOẢN</h3>
+				<form action="" method="post">
+					<p>Tên đăng nhập</p>
+					<input type="text" name="name">
+					<p>Mật khẩu</p>
+					<input type="password" name="password" id=""> <br>
+					<input type="checkbox" name="checkbox" id="">Ghi nhớ tài khoản
+					<input type="submit" value="Đăng nhập">
+				</form>
+				<ul>
+					<li>Quên mật khẩu</li>
+					<li>Đăng ký thành viên</li>
+				</ul>
+			</div>
+			<div class="box">
+				<h3>DANH MỤC</h3>
+
+			</div>
+			<div class="box">
+				<h3>TOP 10 YÊU THÍCH</h3>
+			</div>
+		</div>
 	</div>
 </body>
+
 </html>
